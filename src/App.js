91d864389f921f8b105ac6427bda8df65c3e3723
ChrisMiho <mihovilovich13@gmail.com>
2022-11-12 12:41:09 -0600

    first implementaiton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <header className='App-header'>VAL STATS V.00001</header>
+      <header className='App-header'>Player Name: Rocketlift</header>
+      <div>
+        <form>
+          <label>
+            Player Name:
+            <input type='text' name='name' />
+          </label>
+          <input type='submit' value='Submit' />
+        </form>
+      </div>
+      <div className='player-info'>
+        <Card name='Player Rank' value='Gold 2' />
+        <Card name='Player KD' value={1.52} />
+        <Card name='Player Accuracy' value={'34%'} />
+        <Card name='Ashley' value={24} />
+      </div>
     </div>
   );
 }
